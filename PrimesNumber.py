d7c4b64b1d92d5cf783a2be57d7d3f1c28ee3662
Alessio Lavore <61512398+NotRambi@users.noreply.github.com>
2024-06-04 15:47:28 +0200

    Update PrimesNumber.py
    print every 60 sec;
    change complexity from O(n**2) to O(n*sqrt(n))

--- PrimesNumber.py
+++ PrimesNumber.py
@@ -1,14 +1,25 @@
-i = 1
-num = 1
-flag = 0
+import math
+import time
+
 Prim = [2]
-while i == 1:    
-    for x in range(len(Prim)):
-        if num % Prim[x] == 0 and num != Prim[x]:
-            flag = 1
-    if flag == 0:
-        print (num)
-        if num != 1:
+num = 3
+flag = 0
+t0 = time.time()
+t1 = time.time()
+
+while True:
+        t1 = time.time()
+        r = math.floor(math.sqrt(num))+ 1
+        for x in range(len(Prim)):
+            if Prim[x] > r:
+                break
+            if num % Prim[x] == 0 and num != Prim[x]:
+                flag = 1
+                break
+        if flag == 0:
+            if t1 - t0 > 60:
+                print('last Prime number find: ',num,' number of Prime calculated: ',len(Prim))
+                t0 = t1
             Prim.append(num)
-    flag = 0       
-    num = num + 1
+        flag = 0       
+        num += 1
